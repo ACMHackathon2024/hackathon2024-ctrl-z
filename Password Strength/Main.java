a129859c1a26bc7b6de6a1eedd6ebe93c99011a1
vihuynh0 <165397652+vihuynh0@users.noreply.github.com>
2024-03-29 11:12:56 -0400

    branch test commit

--- Password Strength/Main.java
+++ Password Strength/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Ctrl+ Z's password strength tester!\n\n"
-                            + "Please give us your best password!"
+                        + "Please give us your best password!"
                 + "- Length\n- Alphanumeric\n- Mixed cases"
                             + " and Special Characters: !@#$%^&*()-,./\n");
         System.out.println("Hello World");
 
         //Hello
+        //Hey this is testing branches on github! - Vivian
     }
 }
