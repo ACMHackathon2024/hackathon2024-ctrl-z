19b84a270951c717820d32c8726d09cbfb180215
vihuynh0 <165397652+vihuynh0@users.noreply.github.com>
2024-03-29 11:03:32 -0400

    intro message

--- Password Strength/Main.java
+++ Password Strength/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to Ctrl+ Z's password strength tester!\n\n"
+                            + "Please give us your best password!"
+                + "- Length\n- Alphanumeric\n- Mixed cases"
+                            + " and Special Characters: !@#$%^&*()-,./\n");
         System.out.println("Hello World");
 
         //Hello
