423d91ae88b618d0d11068523f9ffec2e76f5511
TTruong708 <165304729+TTruong708@users.noreply.github.com>
2024-03-29 14:40:17 -0400

    L

--- Q2.c
+++ Q2.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "argon2.h"
+
+struct Encryption {
+    char password[100];
+};
 
 int main(){
+    FILE *file;
 
+    file = fopen("file.txt","r");
+    if (file == NULL) { //Error check for cvsFile
+        printf("Error: File unable to open, please check students.cvs\n");
+        fclose(file);
+        return 1;
+    }
+    
+    close(file);
+    return 0;
 }
