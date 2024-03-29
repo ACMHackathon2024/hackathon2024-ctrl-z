3b9ca08742547ffa68eba4af9118229f4a716684
vihuynh0 <165397652+vihuynh0@users.noreply.github.com>
2024-03-29 11:33:14 -0400

    Fixed scan

--- Password Strength/Main.java
+++ Password Strength/Main.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Ctrl+ Z's password strength tester!\n\n"
-                            + "Please give us your best password!\n"
+                + "Please give us your best password!\n"
                 + "- Length\n- Alphanumeric\n- Mixed cases"
-                            + " and Special Characters: !@#$%^&*()-,./\n");
+                + " and Special Characters: !@#$%^&*()-,./\n");
 
-                            Scanner myObj = new Scanner(System.in);
-                            System.out.println("Please enter password: ");
-                            System.out.println(myObj);
+
+        String password;
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Please enter password: ");
+        password = myObj.nextLine();
+        System.out.println("You entered: " + password + "\n");
+
+        // Tokenize the string using split method:
+        // Read 
                     
 
         //Hello
